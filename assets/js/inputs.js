48958663a0a473eadbc8c3dc168f390a4730b6a6
Robin Fauchery <robin.fauchery.rf@gmail.com>
2023-02-09 14:16:02 +0100

    add form class

--- assets/js/inputs.js
+++ assets/js/inputs.js
@@ -2,33 +2,91 @@ let inputContainers = document.querySelectorAll('.input-container');
 let inputsPassword = document.querySelectorAll('input[type="password"]');
 
 inputContainers.forEach(inputContainer => {
-    let input = inputContainer.querySelector('input');
+    if (inputContainer.querySelectorAll('input').length === 1) {
+        let input = inputContainer.querySelector('input');
 
-    inputContainer.addEventListener('click', () => {
-        inputContainer.classList.add('active');
-        inputContainer.querySelector('input').focus();
-    });
+        inputContainer.addEventListener('click', () => {
+            inputContainer.classList.add('active');
+            inputContainer.querySelector('input').focus();
+        });
+        input.addEventListener('focus', () => {
+            inputContainer.classList.add('active');
+        });
 
-    input.addEventListener('focus', () => {
-        inputContainer.classList.add('active');
-    });
+        input.addEventListener('focus', () => {
+            inputContainer.classList.add('active');
+        });
+        input.addEventListener('blur', () => {
+            if (inputContainer.querySelector('div:hover')) {
+                return;
+            }
+            inputContainer.classList.remove('active');
+        });
 
-    input.addEventListener('blur', () => {
-        if (inputContainer.querySelector('div:hover')) {
-            return;
+        input.addEventListener('blur', () => {
+            if (inputContainer.querySelector('div:hover')) {
+                return;
+            }
+            inputContainer.classList.remove('active');
+        });
+        inputContainer.addEventListener('click', () => {
+            inputContainer.classList.add('active');
+            inputContainer.querySelector('input').focus();
+        });
+    }
+    else if (inputContainer.querySelectorAll('input').length > 1) {
+        let input = inputContainer.querySelectorAll('input');
+
+        input.forEach(input => {
+            input.addEventListener('focus', () => {
+                inputContainer.classList.add('active');
+            });
+
+            input.addEventListener('blur', () => {
+                if (inputContainer.querySelector('div:hover')) {
+                    return;
+                }
+                inputContainer.classList.remove('active');
+            });
+
+            inputContainer.addEventListener('click', (event) => {
+                inputContainer.classList.add('active');
+                if (event.offsetX < inputContainer.offsetWidth / 2) {
+                    inputContainer.querySelector('input').focus();
+                }
+                else {
+                    inputContainer.querySelector('input:last-child').focus();
+                }
+
+            });
+        });
+    }
+    else {
+        let textarea = inputContainer.querySelector('textarea');
+
+        inputContainer.addEventListener('click', () => {
+            inputContainer.classList.add('active');
+            inputContainer.querySelector('textarea').focus();
+        });
+
+        textarea.addEventListener('focus', () => {
+            inputContainer.classList.add('active');
         }
-        inputContainer.classList.remove('active');
-    });
+        );
 
+        textarea.addEventListener('blur', () => {
+            if (inputContainer.querySelector('div:hover')) {
+                return;
+            }
+            inputContainer.classList.remove('active');
+        }
+        );
+    }
 });
 
 inputsPassword.forEach(inputPassword => {
     let inputContainer = inputPassword.closest('.input-container');
     let icon = inputContainer.querySelector('.icon-eye-off');
-
-    console.log(inputPassword)
-    console.log(inputContainer);
-
     icon.addEventListener('click', () => {
         if (inputPassword.type === 'password') {
             inputPassword.type = 'text';
