485e357b20d8d91894e8ab99cd4d029170f84f68
Axel <axel77g@hotmail.com>
2023-03-02 19:40:58 +0100

    [DESTINATION] create map

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ import initSliders from "./slider.js";
 import inputInit from "./inputs.js";
 import componentsInit from "./components/main.js";
 import iconsInit from "./icons.js";
+import mapInit from "./map.js";
 
 (async () => {
   await componentsInit();
@@ -10,4 +11,5 @@ import iconsInit from "./icons.js";
   initSliders();
   inputInit();
   iconsInit();
+  mapInit();
 })();
