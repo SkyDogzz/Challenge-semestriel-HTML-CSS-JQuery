52f7c21fe1685a56746eb76ca573ac4b2d08b691
Axel <axel77g@hotmail.com>
2023-03-05 22:35:56 +0100

    [DESTINATION and GLOBAL] fix some display

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,31 @@ import mapInit from "./map.js";
   inputInit();
   iconsInit();
   mapInit();
+
+  document.querySelectorAll("[scroll-smooth]").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      console.log("ici", this.getAttribute("href").startsWith("#"));
+      if (this.getAttribute("href").startsWith("#")) {
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+        });
+        return;
+      }
+
+      const url = new URL(location.origin + this.getAttribute("href"));
+
+      if (
+        url.pathname.replace(/\/$/gi, "") !=
+        location.pathname.replace(/\/$/gi, "")
+      )
+        location.href = url.href;
+      else {
+        const anchor = url.hash; // "#reviews"
+        document.querySelector(anchor).scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
+  });
 })();
